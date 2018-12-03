0ed5792a62eaac1478d06264b1a9e8908f1e4059
Ilmo Euro <ilmo.euro@gmail.com>
2018-12-03 13:43:47 +0200

    Ask for USB permissions on startup

--- app/src/main/java/fi/metatavu/acgpanel/DefaultActivity.kt
+++ app/src/main/java/fi/metatavu/acgpanel/DefaultActivity.kt
@@ -12,6 +12,12 @@ import android.util.Log
 import android.view.View
 import fi.metatavu.acgpanel.device.McuCommunicationService
 import kotlinx.android.synthetic.main.activity_default.*
+import java.lang.reflect.AccessibleObject.setAccessible
+import android.hardware.usb.UsbDevice
+import android.Manifest.permission
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.content.pm.ApplicationInfo
 
 class DefaultActivity : PanelActivity(lockOnStart = false) {
 
@@ -35,6 +41,42 @@ class DefaultActivity : PanelActivity(lockOnStart = false) {
         }
     }
 
+    @SuppressLint("PrivateApi")
+    private fun grantAutomaticPermission(usbDevice: UsbDevice): Boolean {
+        try {
+            // Use IUsbManager to grant access to device without asking user for permission
+            // This only works if the app is installed in /system/priv-app/
+            val context = this
+            val pkgManager = context.packageManager
+            val appInfo = pkgManager.getApplicationInfo(context.packageName, PackageManager.GET_META_DATA)
+
+            val serviceManagerClass = Class.forName("android.os.ServiceManager")
+            val getServiceMethod = serviceManagerClass.getDeclaredMethod("getService", String::class.java)
+            getServiceMethod.isAccessible = true
+            val binder = getServiceMethod.invoke(null, Context.USB_SERVICE) as android.os.IBinder
+
+            val iUsbManagerClass = Class.forName("android.hardware.usb.IUsbManager")
+            val stubClass = Class.forName("android.hardware.usb.IUsbManager\$Stub")
+            val asInterfaceMethod = stubClass.getDeclaredMethod("asInterface", android.os.IBinder::class.java)
+            asInterfaceMethod.isAccessible = true
+            val iUsbManager = asInterfaceMethod.invoke(null, binder)
+
+            val grantDevicePermissionMethod = iUsbManagerClass.getDeclaredMethod(
+                "grantDevicePermission",
+                UsbDevice::class.java,
+                Int::class.javaPrimitiveType
+            )
+            grantDevicePermissionMethod.isAccessible = true
+            grantDevicePermissionMethod.invoke(iUsbManager, usbDevice, appInfo.uid)
+
+            return true
+        } catch (e: Exception) {
+            // Not a privileged app, fall back to asking permission
+            return false
+        }
+
+    }
+
     private val usbManager: UsbManager
         get() = getSystemService(Context.USB_SERVICE) as UsbManager
 
@@ -56,11 +98,16 @@ class DefaultActivity : PanelActivity(lockOnStart = false) {
     private fun askForMcuPermission() {
         val deviceList = usbManager.deviceList.values
         val device = deviceList.firstOrNull { it.vendorId == CH340G_VENDOR_ID }
+        Log.d(javaClass.name, "requesting permission for $device")
         if (device != null && !usbManager.hasPermission(device)) {
-            usbManager.requestPermission(
-                device,
-                PendingIntent.getBroadcast(this, 0, Intent(ACTION_USB_PERMISSION), 0)
-            )
+            if (!grantAutomaticPermission(device)) {
+                usbManager.requestPermission(
+                    device,
+                    PendingIntent.getBroadcast(this, 0, Intent(ACTION_USB_PERMISSION), 0)
+                )
+            } else {
+                startLockTask()
+            }
         } else {
             startLockTask()
         }
